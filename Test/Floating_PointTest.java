255147cc4c3c39faeec5a856addcd0cf523fc38e
Sergeyprogramm <serg_i_9810@mail.ru>
2017-03-14 12:02:08 +0300

    Sum, Sub, Div, Multi added. created test folder

---
+++ Test/Floating_PointTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by 1081_1 on 14.03.17.
+ */
+public class Floating_PointTest {
+    @org.junit.Test
+    public void getMantis() throws Exception {
+        Floating_Point a = new Floating_Point();
+        a.setDim();
+    }
+
+    @org.junit.Test
+    public void setMantis() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getExp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setExp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void checkForLenght() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void checkForLenghtOfExp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void sum() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void sub() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void div() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void multi() throws Exception {
+
+    }
+
+}
--- src/Floating_Point.java
+++ src/Floating_Point.java
@@ -1,4 +1,8 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.IOException;
+
+import static java.lang.Math.abs;
 
 /**
  * Created by Nikita on 12.02.2017.
@@ -31,7 +35,7 @@ public class Floating_Point {
 
     @Override
     public String toString() {
-        return ""+ mantis + "E" + Exp;
+        return "" + mantis + "E" + Exp;
     }
 
     public int getMantis() {
@@ -50,31 +54,98 @@ public class Floating_Point {
         Exp = exp;
     }
 
-    public boolean checkForLenght(Floating_Point a){
-        String mantis = a.toString().substring(0,a.toString().indexOf("E"));
+    public Dimension getDim() {
+        return dim;
+    }
+
+    public void setDim(Dimension dim) {
+        this.dim = dim;
+    }
+
+    public boolean checkForLenght(Floating_Point a) {
+        String mantis = a.toString().substring(0, a.toString().indexOf("E"));
         String exp = a.toString().substring(a.toString().indexOf("E"));
-        if (!(mantis.length() - a.dim.dimOfMantis <= 0) || !(exp.length()- a.dim.dimOfExp <= 0)){
+        if (!(mantis.length() - a.dim.dimOfMantis <= 0) || !(exp.length() - a.dim.dimOfExp <= 0)) {
             return false;
-        }
-        else {
-            return  true;
+        } else {
+            return true;
         }
     }
-    public boolean checkForLenghtOfExp(Floating_Point a){
-        String mantis = a.toString().substring(0,a.toString().indexOf("E"));
+
+    public boolean checkForLenghtOfExp(Floating_Point a) {
+        String mantis = a.toString().substring(0, a.toString().indexOf("E"));
         String exp = a.toString().substring(a.toString().indexOf("E"));
-        if (!(mantis.length() - a.dim.dimOfMantis <= 0) || !(exp.length()- a.dim.dimOfExp <= 0)){
+        if (!(mantis.length() - a.dim.dimOfMantis <= 0) || !(exp.length() - a.dim.dimOfExp <= 0)) {
             return false;
-        }
-        else {
-            return  true;
+        } else {
+            return true;
         }
     }
-    public void sum (Floating_Point b)throws IOException{
-        if((this.dim.checkForDim(b))&&(checkForLenght(this))&&(checkForLenght(b))){
-            if (this.Exp <= b.Exp){
-                this.mantis = this.mantis*10^(this.Exp-b.Exp)+b.mantis;
-                this.Exp=b.Exp;
+
+    public void sum(Floating_Point b) throws IOException {
+        if ((this.dim.checkForDim(b)) && (checkForLenght(this)) && (checkForLenght(b))) {
+            if (this.Exp <= b.Exp) {
+                this.mantis = this.mantis * 10 ^ (b.Exp - this.Exp) + b.mantis;
+                this.Exp = b.Exp;
+            } else {
+                this.mantis = this.mantis + b.mantis * 10 ^ (this.Exp - b.Exp);
+            }
+            if (!checkForLenght(this)) {
+                IOException e = new IOException();
+                throw e;
+            }
+        } else {
+            IOException e = new IOException();
+            throw e;
+        }
+    }
+
+    public void sub(Floating_Point b) throws IOException {
+        if ((this.dim.checkForDim(b)) && (checkForLenght(this)) && (checkForLenght(b))) {
+            if (this.Exp <= b.Exp) {
+                this.mantis = this.mantis * 10 ^ (b.Exp - this.Exp) - b.mantis;
+                this.Exp = b.Exp;
+            } else {
+                this.mantis = this.mantis - b.mantis * 10 ^ (this.Exp - b.Exp);
+            }
+            if (!checkForLenght(this)) {
+                IOException e = new IOException();
+                throw e;
+            }
+        } else {
+            IOException e = new IOException();
+            throw e;
+        }
+    }
+    public void div(Floating_Point b) throws IOException{
+        if ((this.dim.checkForDim(b)) && (checkForLenght(this)) && (checkForLenght(b))) {
+            double addmantis = this.mantis / b.mantis;
+            int addExp = this.Exp - b.Exp;
+            while (abs(addmantis) < 1) {
+                addmantis = addmantis * 10;
+                addExp = addExp - 1;
+            }
+            this.mantis = (int)addmantis;
+            this.Exp = addExp;
+            if (!checkForLenght(this)){
+                IOException e = new IOException();
+                throw e;
+            }
+        }
+        else {
+            IOException e = new IOException();
+            throw e;
+        }
+    }
+    public void multi (Floating_Point b) throws IOException{
+        if ((this.dim.checkForDim(b)) && (checkForLenght(this)) && (checkForLenght(b))) {
+            double addmantis = this.mantis * b.mantis;
+            int addExp = this.Exp + b.Exp;
+            this.mantis = (int)addmantis;
+            this.Exp = addExp;
+            if (!checkForLenght(this)){
+                IOException e = new IOException();
+                throw e;
             }
         }
         else {
