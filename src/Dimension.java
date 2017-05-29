5b7a9b520c7c10a564aacfbbfa400232b2165162
SidorenkoNikita <bioshock@list.ru>
2017-05-30 00:38:57 +0300

    Work in progress/ Dimension compleated on 80%/Flating point 60%

--- src/Dimension.java
+++ src/Dimension.java
@@ -1,11 +1,62 @@
 import java.io.IOException;
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Nikita on 12.02.2017.
  */
 public class Dimension {
-    public int dimOfExp;
-    public int dimOfMantis;
+    /*
+    Правила написания размерности: ключом служит величина, значением - приставка степени
+    Величина указывается в базовым формате СИ
+    Работа с более сложными видами единиц in progress
+    */
+    private HashMap<String,String> dimOfNumenator;
+    private HashMap<String,String> dimOfDenominator;
+
+    private HashMap<String,Integer> translationTable;
+    private HashMap<String,String> exceptionTable;
+
+    public HashMap<String, String> getDimOfNumenator() {
+        return dimOfNumenator;
+    }
+
+    public void setDimOfNumenator(HashMap<String, String> dimOfNumenator) {
+        this.dimOfNumenator = dimOfNumenator;
+    }
+
+    public HashMap<String, String> getDimOfDenominator() {
+        return dimOfDenominator;
+    }
+
+    public void setDimOfDenominator(HashMap<String, String> dimOfDenominator) {
+        this.dimOfDenominator = dimOfDenominator;
+    }
+
+    public HashMap<String, Integer> getTranslationTable() {
+        return translationTable;
+    }
+
+    public void setTranslationTable(HashMap<String, Integer> translation) {
+        this.translationTable = translation;
+    }
+
+    public int getOrder(String key) {
+        if (translationTable.containsKey(key)) {
+            translationTable.get(key);
+        }
+    }
+
+    public HashMap<String, String> getExceptionTable() {
+        return exceptionTable;
+    }
+
+    public void setExceptionTable(HashMap<String, String> exceptionTable) {
+        this.exceptionTable = exceptionTable;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -14,49 +65,48 @@ public class Dimension {
 
         Dimension dimension = (Dimension) o;
 
-        if (dimOfExp != dimension.dimOfExp) return false;
-        return dimOfMantis == dimension.dimOfMantis;
+        if (dimOfNumenator != null ? !dimOfNumenator.equals(dimension.dimOfNumenator) : dimension.dimOfNumenator != null)
+            return false;
+        if (dimOfDenominator != null ? !dimOfDenominator.equals(dimension.dimOfDenominator) : dimension.dimOfDenominator != null)
+            return false;
+        if (translationTable != null ? !translationTable.equals(dimension.translationTable) : dimension.translationTable != null)
+            return false;
+        return exceptionTable != null ? exceptionTable.equals(dimension.exceptionTable) : dimension.exceptionTable == null;
 
     }
 
     @Override
     public int hashCode() {
-        int result = dimOfExp;
-        result = 31 * result + dimOfMantis;
+        int result = dimOfNumenator != null ? dimOfNumenator.hashCode() : 0;
+        result = 31 * result + (dimOfDenominator != null ? dimOfDenominator.hashCode() : 0);
+        result = 31 * result + (translationTable != null ? translationTable.hashCode() : 0);
+        result = 31 * result + (exceptionTable != null ? exceptionTable.hashCode() : 0);
         return result;
     }
 
     @Override
     public String toString() {
-        return "Dimension{" +
-                "dimOfExp=" + dimOfExp +
-                ", dimOfMantis=" + dimOfMantis +
-                '}';
-    }
+        String result = "";
+        int i;
+        for (Map.Entry entry: dimOfNumenator.entrySet()) {
 
-    public int getDimOfExp() {
-        return dimOfExp;
-    }
-
-    public void setDimOfExp(int dimOfExp) {
-        this.dimOfExp = dimOfExp;
-    }
-
-    public int getDimOfMantis() {
-        return dimOfMantis;
-    }
-
-    public void setDimOfMantis(int dimOfMantis) {
-        this.dimOfMantis = dimOfMantis;
-    }
-
-    public boolean checkForDim(Floating_Point b){
-        if(b.dim.equals(this)) {
-            return  true;
+            Key K= entry.getKey();
+            Value V = entry.getValue();
+            result = K+V;
         }
-        else {
-            return false;
+        result = result + "/";
+        for (Map.Entry entry: dimOfDenominator.entrySet()) {
+
+            Key K= entry.getKey();
+            Value V = entry.getValue();
+            result += K+V;
         }
     }
+    public int toBase(){
 
+    }
+    public void fromString(String a){
+        String[] parts = a.split("/");
+        this.dimOfNumenator =
+    }
 }
--- src/Floating_Point.java
+++ src/Floating_Point.java
@@ -1,17 +1,14 @@
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
-import java.io.IOException;
+import java.util.ArrayList;
 
 import static java.lang.Math.abs;
-import static  java.lang.Math.pow;
+import java.lang.String;
 
 /**
  * Created by Nikita on 12.02.2017.
  */
 public class Floating_Point {
-    public int mantis;
-    public int Exp;
-    public Dimension dim;
+    private Double number;
+    private Dimension dim;
 
     @Override
     public boolean equals(Object o) {
@@ -20,138 +17,53 @@ public class Floating_Point {
 
         Floating_Point that = (Floating_Point) o;
 
-        if (mantis != that.mantis) return false;
-        if (Exp != that.Exp) return false;
+        if (number != null ? !number.equals(that.number) : that.number != null) return false;
         return dim != null ? dim.equals(that.dim) : that.dim == null;
 
     }
 
     @Override
     public int hashCode() {
-        int result = mantis;
-        result = 31 * result + Exp;
+        int result = number != null ? number.hashCode() : 0;
         result = 31 * result + (dim != null ? dim.hashCode() : 0);
         return result;
     }
-
-    @Override
-    public String toString() {
-        return "" + mantis + "E" + Exp;
-    }
-
-    public int getMantis() {
-        return mantis;
-    }
-
-    public void setMantis(int mantis) {
-        this.mantis = mantis;
-    }
-
-    public int getExp() {
-        return Exp;
-    }
-
-    public void setExp(int exp) {
-        Exp = exp;
-    }
-
-    public Dimension getDim() {
-        return dim;
-    }
-
-    public void setDim(Dimension dim) {
-        this.dim = dim;
-    }
-
-    public boolean checkForLenght() {
-        String mantis = this.toString().substring(0, this.toString().indexOf("E")-1);
-        String exp = this.toString().substring(this.toString().indexOf("E")+1);
-        if (!(mantis.length() - this.dim.dimOfMantis <= 0) || !(exp.length() - this.dim.dimOfExp <= 0)) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    public boolean checkForLenghtOfExp() {
-        String mantis = this.toString().substring(0, this.toString().indexOf("E"));
-        String exp = this.toString().substring(this.toString().indexOf("E"));
-        if (!(mantis.length() - this.dim.dimOfMantis <= 0) || !(exp.length() - this.dim.dimOfExp <= 0)) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    public void sum(Floating_Point b) throws IOException {
-        if ((this.dim.checkForDim(b)) && (this.checkForLenght()) && (b.checkForLenght())) {
-            if (this.Exp <= b.Exp) {
-                this.mantis = (int)(this.mantis + b.mantis * pow (10,(b.Exp - this.Exp)));
-            } else {
-                this.mantis = (int)(this.mantis * pow  (10,this.Exp - b.Exp) + b.mantis);
-                this.Exp = this.Exp - b.Exp;
-            }
-            if (!this.checkForLenght()) {
-                IOException e = new IOException();
-                throw e;
-            }
-        } else {
-            IOException e = new IOException();
-            throw e;
-        }
-    }
-
-    public void sub(Floating_Point b) throws IOException {
-        if ((this.dim.checkForDim(b)) && (this.checkForLenght()) && (b.checkForLenght())) {
-            if (this.Exp <= b.Exp) {
-                this.mantis = (int)(this.mantis  - b.mantis*  pow (10,b.Exp - this.Exp));
-                this.Exp = b.Exp;
-            } else {
-                this.mantis = (int)(this.mantis *pow (10,b.Exp - this.Exp) - b.mantis) ;
-            }
-            if (!this.checkForLenght()) {
-                IOException e = new IOException();
-                throw e;
-            }
-        } else {
-            IOException e = new IOException();
-            throw e;
-        }
-    }
-    public void div(Floating_Point b) throws IOException{
-        if ((this.dim.checkForDim(b)) && (this.checkForLenght()) && (b.checkForLenght())) {
-            double addmantis = this.mantis / b.mantis;
-            int addExp = this.Exp - b.Exp;
-            while (abs(addmantis) < 1) {
-                addmantis = addmantis * 10;
-                addExp = addExp - 1;
-            }
-            this.mantis = (int)addmantis;
-            this.Exp = addExp;
-            if (!this.checkForLenght()){
-                IOException e = new IOException();
-                throw e;
-            }
+    public void sum (Floating_Point a) throws Exception{
+        a.toBase();
+        if (this.dim.equals(a.dim)){
+            this.number +=a.number;
         }
         else {
-            IOException e = new IOException();
-            throw e;
+            throw new Exception("Not compatible dimmension");
         }
     }
-    public void multi (Floating_Point b) throws IOException{
-        if ((this.dim.checkForDim(b)) && (this.checkForLenght()) && (b.checkForLenght())) {
-            double addmantis = this.mantis * b.mantis;
-            int addExp = this.Exp + b.Exp;
-            this.mantis = (int)addmantis;
-            this.Exp = addExp;
-            if (!this.checkForLenght()){
-                IOException e = new IOException();
-                throw e;
-            }
+    public void sub (Floating_Point a) throws Exception{
+        if (this.dim.equals(a.dim)){
+            this.number -=a.number;
         }
         else {
-            IOException e = new IOException();
-            throw e;
+            throw new Exception("Not compatible dimmension");
         }
+    }
+    public void multi (Floating_Point a) throws Exception{
+        if (this.dim.equals(a.dim)){
+            this.number *=a.number;
+        }
+        else {
+            throw new Exception("Not compatible dimmension");
+        }
+    }
+    public void div (Floating_Point a) throws Exception{
+        if (this.dim.equals(a.dim)){
+            this.number /=a.number;
+        }
+        else {
+            throw new Exception("Not compatible dimmension");
+        }
+    }
+    public String readFromString(String a){
+        String[] parts = a.split(" ",2);
+        this.number = Double.parseDouble(parts[1]);
+        this.dim.fromString(parts[2]);
     }
 }
